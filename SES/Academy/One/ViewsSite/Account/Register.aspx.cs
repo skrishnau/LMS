9659f11b67a5f668d99c7d04a06919be602ec116
kri <skrishnau@gmail.com>
2016-06-29 18:28:37 -0700

    register.aspx

---
+++ SES/Academy/One/ViewsSite/Account/Register.aspx.cs
@@ -0,0 +1,121 @@
+﻿using Academic.DbHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Academic.DbEntities.User;
+using One.Values.MemberShip;
+
+namespace One.ViewsSite.Account
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //CreateUserWizard1.
+        }
+
+        protected void CreateUserWizard1_CreatedUser(object sender, EventArgs e)
+        {
+            var username = CreateUserWizard1.UserName;
+            var password = CreateUserWizard1.Password;
+            var email = CreateUserWizard1.Email;
+            var que = CreateUserWizard1.Question;
+            var ans = CreateUserWizard1.Answer;
+            var user = new Academic.DbEntities.User.Users()
+            {
+                UserName = CreateUserWizard1.UserName
+                ,
+                Password = CreateUserWizard1.Password
+                ,
+                Email = CreateUserWizard1.Email
+                ,
+                SecurityQuestion = CreateUserWizard1.Question
+                ,
+                SecurityAnswer = CreateUserWizard1.Answer
+
+            };
+
+            using (var helper = new DbHelper.CustomAccount())
+            {
+                var success = helper.Register(user);
+                if (success)
+                    UserLogin(user);
+            }
+        }
+
+        public void UserLogin(Users viewModel)
+        {
+            using (var acchelper = new DbHelper.CustomAccount())
+            {
+                //if (Membership.ValidateUser(viewModel.UserName, viewModel.Password))
+                if (acchelper.CheckUser(viewModel.UserName, viewModel.Password))
+                {
+                    using (var acaHelper = new DbHelper.AcademicYear())
+                    using (var helper = new DbHelper.User())
+                    {
+                        var user = helper.Users.First(u => u.UserName == viewModel.UserName);
+
+                        CustomPrincipalSerializeModel serializeModel = new CustomPrincipalSerializeModel();
+                        serializeModel.Id = user.Id;
+                        serializeModel.FirstName = user.FirstName;
+                        serializeModel.LastName = user.LastName;
+
+                        serializeModel.SchoolId = user.SchoolId ?? 0;
+                        var acaId = acaHelper.GetCurrentAcademicYear(user.SchoolId ?? 0);
+                        if (acaId != null)
+                        {
+                            serializeModel.AcademicYearId = acaId.Id;
+                            var sess = acaHelper.GetCurrentSession(acaId.Id);
+                            if (sess != null)
+                            {
+                                serializeModel.SessionId = sess.Id;
+                            }
+                            //else
+                            //{
+                            //    serializeModel.SessionId = 0;
+                            //}
+                        }
+
+                        JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                        string userData = serializer.Serialize(serializeModel);
+
+                        FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                            1,
+                            //viewModel.Email,
+                            viewModel.UserName,
+                            DateTime.Now,
+                            DateTime.Now.AddMinutes(15),
+                            false,
+                            userData);
+
+                        string encTicket = FormsAuthentication.Encrypt(authTicket);
+                        HttpCookie faCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
+                        Response.Cookies.Add(faCookie);
+
+                        //FormsAuthentication.set
+                        //return RedirectToAction("Index", "Home");
+                        string returnUrl = Request.QueryString["ReturnUrl"] as string;
+                        if (returnUrl != null)
+                        {
+                            //if (returnUrl.Contains("DashBoard%2fStudent") && roles.Contains("student"))
+                            //{
+
+                            //}
+                            Response.Redirect(returnUrl);
+                        }
+                        else
+                        {
+                            Response.Redirect("~/ViewsSite/Default.aspx");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
