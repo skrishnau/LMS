6ea076b5db9ff1a5dab9f928569d171757712ebc
kri <skrishnau@gmail.com>
2016-08-21 07:15:24 -0700

    subject_user_group

---
+++ SES/Academic.DbEntities/Subjects/SubjectUserGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academic.DbEntities.Subjects
+{
+    //will be used ... it is used to override the default grouping of students in a course
+    // so the grouping of students will be as per this setting.
+   public class SubjectUserGroup
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+       
+       public int SubjectId { get; set; }
+       public virtual Subjects.Subject Subject { get; set; }
+
+
+
+    }
+}
